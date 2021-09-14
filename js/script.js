dd58db3b521b9fcbf3a0846354852c33c46adec4
jianylei <jianyonglei@gmail.com>
2021-09-14 13:36:00 -0400

    updated portoflio item to show "in-card"

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const aboutSection = document.querySelector(".about-section");
 const aboutText = document.querySelector(".about-text");
 const contactSection = document.querySelector(".contact-section");
 const portfolioSection = document.querySelector(".portfolio-section");
+const portfolioItem = document.querySelector(".pp-item-section");
 
 tabsContainer.onclick = (e) => {
     if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
@@ -14,73 +15,99 @@ tabsContainer.onclick = (e) => {
         aboutSection.querySelector(target).classList.add("active");
     }
 }
-document.querySelector(".btn-contact").onclick = (e) => {
-    returnHome(true, false)
+//go to contacts
+document.querySelector(".btn-contact").onclick = () => {
+    togglePopup("contact");
 }
+//go to portfolio
 document.querySelector(".btn-portfolio").onclick = () => {
-    returnHome(false, false)
+    togglePopup("portfolio");
 }
 
 //portfolio item popup
 document.onclick = (e) => {
     if (e.target.classList.contains("view-project-btn")) {
         togglePortfolioPopup();
-        document.querySelector(".portfolio-popup").scrollTo(0, 0);
         portfolioItemDetails(e.target.parentElement);
     }
 }
 function togglePortfolioPopup() {
-    document.querySelector(".portfolio-popup").classList.toggle("open");
-    document.querySelector(".main-container").classList.toggle("hidden");
-    document.body.classList.toggle("hide-scroll");
+    togglePopup("item");
+    portfolioItem.classList.toggle("hidden");
+    portfolioSection.classList.toggle("hidden");
 }
-document.querySelector(".pp-close").onclick = togglePortfolioPopup;
-document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("pp-inner")) {
-        togglePortfolioPopup();
-    }
-})
 //set item details
 function portfolioItemDetails(portfolioItem) {
     document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
     document.querySelector(".pp-header h3").innerText = portfolioItem.querySelector("h3").innerText;
     document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
-//close portfolio
-document.querySelector(".portfolio-close").onclick = () => {
-    returnHome(false, true);
-}
-//contact
-//close contact
-document.querySelector(".section-close").onclick = () => {
-    returnHome(true, true);
-}
-function returnHome(isContact, returnAbout) {
-    if (isContact) {
-        contactSection.classList.toggle("hidden");
-        if (!portfolioSection.classList.contains("hidden")) {
-            portfolioSection.classList.toggle("hidden");
-        }
+
+//close buttons
+document.querySelectorAll(".close").forEach(btn => {
+    btn.onclick = () => {
+        togglePopup("close");
     }
-    else {
-        portfolioSection.classList.toggle("hidden");
-        if (!contactSection.classList.contains("hidden")) {
-            contactSection.classList.toggle("hidden");
-        }
-    }
-    if (aboutText.classList.contains("hidden")) {
-        if (returnAbout) {
+})
+
+function togglePopup(section) {
+    if(section === "contact"){
+        if (!aboutText.classList.contains("hidden")){
             aboutText.classList.toggle("hidden");
         }
-    } else {
-        aboutText.classList.toggle("hidden");
+        if (!portfolioSection.classList.contains("hidden")){
+            portfolioSection.classList.toggle("hidden");
+        }
+        if (!portfolioItem.classList.contains("hidden")){
+            portfolioItem.classList.toggle("hidden");
+        }
+    
+        if(contactSection.classList.contains("hidden")){
+            contactSection.classList.toggle("hidden")
+        }
     }
 
-    if (aboutText.classList.contains("hidden") && portfolioSection.classList.contains("hidden") && contactSection.classList.contains("hidden")) {
-        if (isContact) {
+    if(section === "portfolio"){
+        if (!aboutText.classList.contains("hidden")){
+            aboutText.classList.toggle("hidden");
+        }
+        if (!contactSection.classList.contains("hidden")){
             contactSection.classList.toggle("hidden");
-        } else {
-            portfolioSection.classList.toggle("hidden");
+        }
+        if (!portfolioItem.classList.contains("hidden")){
+            portfolioItem.classList.toggle("hidden");
+        }
+
+        if(portfolioSection.classList.contains("hidden")){
+            portfolioSection.classList.toggle("hidden")
+        }
+    }
+
+    if(section === "item"){
+        if (!aboutText.classList.contains("hidden")){
+            aboutText.classList.toggle("hidden");
+        }
+        if (!contactSection.classList.contains("hidden")){
+            contactSection.classList.toggle("hidden");
+        }
+        if(portfolioSection.classList.contains("hidden")){
+            portfolioSection.classList.toggle("hidden")
+        }
+    }
+
+    if(section === "close"){
+        if (!contactSection.classList.contains("hidden")){
+            contactSection.classList.toggle("hidden");
+        }
+        if (!portfolioItem.classList.contains("hidden")){
+            portfolioItem.classList.toggle("hidden");
+        }
+        if(!portfolioSection.classList.contains("hidden")){
+            portfolioSection.classList.toggle("hidden")
+        }
+
+        if (aboutText.classList.contains("hidden")){
+            aboutText.classList.toggle("hidden");
         }
     }
 }
